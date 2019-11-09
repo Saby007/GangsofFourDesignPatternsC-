5bd730d9aff21fcce17ae8b5b561c3354ec05bff
Sabyasachi Samaddar <sabyanew@gmail.com>
2019-11-10 00:18:30 +0530

    InterpreterPattern

---
+++ GOFDP/BehavioralPatterns/InterpreterPattern/Number.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+/// <summary>
+/// The Number Terminal expression class
+/// </summary>
+public class Number : RPNExpression
+{
+	
+}
---
+++ GOFDP/BehavioralPatterns/InterpreterPattern/RPNExpression.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// The abstract RPN Expression Class
+/// </summary>
+public abstract class RPNExpression
+{
+    /// <summary>
+    /// Interpret the expressions
+    /// </summary>
+    /// <returns>value of the interpreter</returns>
+    public abstract int Interpret();
+}
